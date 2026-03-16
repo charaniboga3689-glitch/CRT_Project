ab77e8a574c5776e106a557783aaadf6f322dfd3
charaniboga3689 <charaniboga3689@gmail.com>
2026-03-16 19:39:47 +0530

    msg

--- IV-SEM/Python/Practice/M03_Core_Concepts/S02/PS02_Python_Collection.py
+++ IV-SEM/Python/Practice/M03_Core_Concepts/S02/PS02_Python_Collection.py
@@ -64,5 +64,84 @@ class Solution:
             
         return candidate
 
-'''
+#2) Creation of set:
+a = {1, 2, 3, 4, 5, 6}
+print(a)
+b = set([1, 2, 3, 45, 5])
+print(b)
 
+#3) adding elements to set:
+b = set([1,2,3,45,5])
+b.add(50)
+print(b)
+
+#4) Removing 
+b = set([1,2,3,45,5])
+b.remove(45)
+print(b)
+
+#5) Set Operations:
+a = {1,2,3,5,6}
+b = {10,2,3,5,60}
+print(a.union(b))
+print(a.intersection(b))
+print(a.difference(b))
+
+#Leet code: 268. Missing numbers
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+Example 1:
+Input: nums = [3,0,1]
+Output: 2
+Explanation:
+n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+Example 2:
+Input: nums = [0,1]
+Output: 2
+Explanation:
+n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+Example 3:
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation:
+n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+Constraints:
+n == nums.length
+1 <= n <= 104
+0 <= nums[i] <= n
+All the numbers of nums are unique.
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+code:
+class Solution:
+    def missingNumber(self, nums: list[int]) -> int:
+        n = len(nums)
+        total = n * (n + 1) // 2
+        return total - sum(nums)
+
+
+575. Distribute Candies
+
+Alice has n candies, where the ith candy is of type candyType[i]. Alice noticed that she started to gain weight, so she visited a doctor.
+
+The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). Alice likes her candies very much, and she wants to eat the maximum number of different types of candies while still following the doctor's advice.
+
+Given the integer array candyType of length n, return the maximum number of different types of candies she can eat if she only eats n / 2 of them.
+Example 1:
+Input: candyType = [1,1,2,2,3,3]
+Output: 3
+Explanation: Alice can only eat 6 / 2 = 3 candies. Since there are only 3 types, she can eat one of each type.
+Example 2:
+Input: candyType = [1,1,2,3]
+Output: 2
+Explanation: Alice can only eat 4 / 2 = 2 candies. Whether she eats types [1,2], [1,3], or [2,3], she still can only eat 2 different types.
+Example 3:
+Input: candyType = [6,6,6,6]
+Output: 1
+Explanation: Alice can only eat 4 / 2 = 2 candies. Even though she can eat 2 candies, she only has 1 type.
+
+code:
+from typing import List
+
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        return min(len(set(candyType)), len(candyType)//2)
+'''
